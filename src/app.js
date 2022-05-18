86fb23e97b24acdf2ffcccbd58761d1eb77f2ea5
Kamerr Ezz <45726492+KamerrEzz@users.noreply.github.com>
2022-05-18 12:55:15 -0500

    Middleware errors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const app = express();
 
+// Middleware Import
+const bodyparser = require("body-parser");
+const ErrorHander = require("./utils/middleware/error.middleware");
+
 module.exports = (port) => {
+  app.use(bodyparser.json());
+  app.use(bodyparser.urlencoded({ extended: false }));
+
+  app.use(ErrorHander.logErrors);
+  app.use(ErrorHander.boomError);
+  app.use(ErrorHander.defaultError);
+
   app.listen(port, () => {
     console.log(`listen ${port}`);
   });
