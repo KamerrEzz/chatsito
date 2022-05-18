1794c3b18cd94aa5467b4b047ede4d7d24c3041e
Kamerr Ezz <45726492+KamerrEzz@users.noreply.github.com>
2022-05-18 15:54:05 -0500

    message router

--- src/routes/message.routes.js
+++ src/routes/message.routes.js
@@ -1,13 +1,18 @@
 const { Router } = require("express");
 
-const { getMessages, addMessage } = require("../controller/message.controller");
+const {
+  getMessages,
+  addMessage,
+  updateMessage,
+  deleteMessage,
+} = require("../controller/message.controller");
 const { success } = require("../utils/network/response");
 
 const routes = new Router();
 
 // Add routes
 routes.get("/", (req, res, next) => {
-  const filterMessage = req.query.user || null
+  const filterMessage = req.query.user || null;
   getMessages(filterMessage)
     .then((msg) => success(req, res, msg))
     .catch((e) => {
@@ -19,7 +24,17 @@ routes.post("/", (req, res, next) => {
     .then((msg) => success(req, res, msg))
     .catch((e) => next(e));
 });
-// routes.put('/', SessionController.store);
-// routes.delete('/', SessionController.store);
+routes.put("/:id", (req, res, next) => {
+  const id = req.params.id;
+  updateMessage(id, req.body)
+    .then((msg) => success(req, res, msg))
+    .catch((e) => next(e));
+});
+routes.delete("/:id", (req, res, next) => {
+  const id = req.params.id;
+  deleteMessage(id)
+    .then((msg) => success(req, res, msg))
+    .catch((e) => next(e));
+});
 
 module.exports = routes;
