46ded6384c16308a8407e4d50c18d865c1407192
Kamerr Ezz <45726492+KamerrEzz@users.noreply.github.com>
2022-05-18 13:52:54 -0500

    mongoose connect

--- src/controller/message.controller.js
+++ src/controller/message.controller.js
@@ -2,7 +2,9 @@ const boom = require('@hapi/boom');
 
 const getMessages = () => {
     return new Promise((resolve, reject) => {
-        reject(boom.badGateway())
+        resolve({
+            hola: "hola"
+        })
     })
 }
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-const APP = require('./app');
+const APP = require("./app");
+const mongoose = require("mongoose");
+const { mongodb } = require("./config");
+
+mongoose
+  .connect(mongodb)
+  .then(() => console.log("mongoose conectado"))
+  .catch(console.log);
 
 APP(3000);
